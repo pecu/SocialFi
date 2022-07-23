617f48dc734cd947067081d3676d1529c60bb98e
rurutoria7 <nandokabeam@gmail.com>
2022-07-23 19:39:29 +0800

    Update main.py

--- dcbot-sample-code/main.py
+++ dcbot-sample-code/main.py
@@ -13,11 +13,25 @@ async def on_ready():
     c = bot.get_channel(channel_id)
     await c.send('>> Bot is on ready <<')
 
+@bot.event
+async def say_hi (ctx: cmds.Context):
+    await ctx.send('starbuststream')    # change it
+
 img_path = ['imgs\\sample_img1.png', 'imgs\\sample_img2.png']    # Change it
 
 @bot.command()
-async def local_img(ctx: cmds.Context, id: int):
+async def img(ctx: cmds.Context, id: int):
     pic = dc.File(img_path[id])
-    await ctx.send(file=pic) 
+    await ctx.send(file=pic)
+
+captions = ['i tried so hard', 'and got so far']     # Change it
+
+@bot.command()
+async def img_with_caption(ctx: cmds.Context, id: int):   # Finish it
+    # pic = dc.File(img_path[id])   # 選擇圖片
+    # await ctx.send( ___(A)___ )   # 印出圖片
+    # cap = ___(B)___               # 選擇文字
+    # await ctx.send(cap)           # 印出文字
+    pass
 
 bot.run(token)
